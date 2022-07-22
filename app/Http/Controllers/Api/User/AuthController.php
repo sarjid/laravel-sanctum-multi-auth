fa507b3264497c5d70e74a6c8be0eb052c5cb2d6
sarjid <sarjid777gmail.com>
2022-07-23 02:58:25 +0600

    first commit

---
+++ app/Http/Controllers/Api/User/AuthController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Api\User;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\User\LoginRequest;
+use App\Http\Requests\User\RegisterRequest;
+use App\Http\Resources\User\AuthResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function login(LoginRequest $request)
+    {
+        $user = User::where('phone', $request->phone)->first();
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        return $this->makeToken($user);
+    }
+
+
+    public function register(RegisterRequest $request)
+    {
+        $user = User::create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'phone' => $request->input('phone'),
+            'password' => bcrypt($request->input('password')),
+        ]);
+
+        return $this->makeToken($user);
+    }
+
+    public function makeToken($user)
+    {
+        $token =  $user->createToken('user-token')->plainTextToken;
+        return (new AuthResource($user))
+            ->additional(['meta' => [
+                'token' => $token,
+                'token_type' => 'Bearer',
+            ]]);
+    }
+}
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Models\User::factory(10)->create();
+
+        \App\Models\User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@gmail.com',
+            'phone' => '0172260010',
+            'password' => Hash::make('password'),
+        ]);
+    }
+}
