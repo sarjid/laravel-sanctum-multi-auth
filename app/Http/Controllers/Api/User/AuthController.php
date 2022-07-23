0f32d6f675b7db4baade1de79684d241df305a87
sarjid <sarjid777gmail.com>
2022-07-23 15:17:42 +0600

    logout

--- app/Http/Controllers/Api/User/AuthController.php
+++ app/Http/Controllers/Api/User/AuthController.php
@@ -47,4 +47,16 @@ class AuthController extends Controller
                 'token_type' => 'Bearer',
             ]]);
     }
+
+
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+        return send_ms('User Logoout', true, 200);
+    }
+
+    public function user(Request $request)
+    {
+        return AuthResource::make($request->user());
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -15,14 +15,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', [AuthController::class, 'user']);
 });
-
-
-
-Route::post('/login',[AuthController::class,'login']);
-Route::post('/register',[AuthController::class,'register']);
-
-
-
